d80d588bd84f575ea86256764b25647f9944e5b8
Micael Macedo <micaelm2009@hotmail.com>
2023-02-24 12:08:40 -0300

    feat: add api removebg

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,10 @@ async function buscarImagem() {
     id = $("#imgId").val()
     $(".imagens").empty();
     await fetch(`http://localhost:8090/foto/imagens/${id}`).then(response => response.json()).then(imagem => {
-        $(".imagens").append(`<img src="http://localhost:8090/images/${imagem.caminho}" alt="${imagem.nomeArquivo}">`)
+        $(".imagens").append(`
+        <img src="http://localhost:8090/images/${imagem.caminho}" alt="${imagem.nomeArquivo}" id="imagem">
+        <button onclick="removerfundo(${imagem.id})">Remover Fundo</Button>
+        `)
     })
 }
 async function buscarImagemUsuario() {
@@ -13,4 +16,24 @@ async function buscarImagemUsuario() {
             $(".imagens").append(`<img src="http://localhost:8090/images/${imagem.caminho}" alt="${imagem.nomeArquivo}">`)
         });
     })
+}
+
+async function removerfundo(idImg) {
+    var myHeaders = new Headers();
+    var myInit = {
+        method: 'get',
+        headers: myHeaders,
+    };
+    await fetch(`http://localhost:8090/foto/removerfundo/${idImg}`, myInit)
+        .then(response => response.blob())
+        .then(data => {
+            const url = URL.createObjectURL(data)
+            let img = $("#imagemBgRemovida");
+            img.onload = () => {
+                URL.revokeObjectURL(url)
+                resolve(img)
+            }
+            img.src = url
+        })
+        .catch(err => console.log(err))
 }
--- router/Foto.js
+++ router/Foto.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const fetch = require("node-fetch")
 const db = require("../mysql")
 const axios = require('axios');
 const FormData = require('form-data');
 const fs = require('fs');
-
-const formData = new FormData();
-formData.append('size', 'auto');
-formData.append('image_url', 'https://www.remove.bg/example.jpg');
+const path = require('path')
 
 const inputPath = "./public/images/";
 
@@ -34,25 +32,56 @@ router.get('/', (req, res, next) => {
 })
 router.post("/", upload.single('image'), (req, res, next) => {
     console.log(req.file);
-
-    const formData = new FormData();
-    formData.append('size', 'auto');
-    formData.append('image_url', `http://localhost:8090/public/images/${req.file.destination}`);
-    
-
     db.cadastrarImagens(req.body.idUser, req.file.originalname, req.file.filename)
     res.redirect("http://localhost:8090/");
 })
+router.get("/removerfundo/:id", async (req, res, next) => {
+    // const inputPath = path.join(process.cwd(), `public/images/${imagem.caminho}`);
+    let result = await fetch('https://api.remove.bg/v1.0/removebg', {
+        method: 'POST',
+        headers: {
+            Accept: '*/*',
+            'X-Api-Key': 'ciunUWx66wTKKErDT9ubprAh',
+            'Content-Type': 'application/json'
+        },
+        body: '{"image_url":"https://www.remove.bg/example.jpg","size":"auto"}'
+    }).then(response => { 
+        if (response.status != 200) return console.error('Error:', response.status, response.statusText);
+        return response.blob();
+    }).catch(error => {
+        return console.error('Request failed:', error);
+    });
+    res.send((result));
+});
+router.post("/removerfundo/:id", async (req, res, next) => {
+    // const inputPath = path.join(process.cwd(), `public/images/${imagem.caminho}`);
+    let result = await fetch('https://api.remove.bg/v1.0/removebg', {
+        method: 'POST',
+        headers: {
+            Accept: '*/*',
+            'X-Api-Key': 'ciunUWx66wTKKErDT9ubprAh',
+            'Content-Type': 'application/json'
+        },
+        body: '{"image_url":"https://www.remove.bg/example.jpg","size":"auto"}'
+    }).then(response => { 
+        if (response.status != 200) return console.error('Error:', response.status, response.statusText);
+        return response.type;
+    }).catch(error => {
+        return console.error('Request failed:', error);
+    });
+    res.send(result);
+});
+
 router.get("/imagens/:id", (req, res, next) => {
-    db.getImagemById(req.params.id, function(err,imagens) {
-        if(err){res.status(404)}
-        else{ res.status(200).json(imagens)} 
+    db.getImagemById(req.params.id, function (err, imagens) {
+        if (err) { res.status(404) }
+        else { res.status(200).json(imagens) }
     })
 });
 router.get("/imagens/user/:id", (req, res, next) => {
-    db.getImagemByUser(req.params.id, function(err,imagens) {
-        if(err){res.status(404)}
-        else{ res.status(200).json(imagens)} 
+    db.getImagemByUser(req.params.id, function (err, imagens) {
+        if (err) { res.status(404) }
+        else { res.status(200).json(imagens) }
     })
 });
 
